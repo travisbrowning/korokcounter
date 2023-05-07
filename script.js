4ec24955cf0ed543e61290b10a655bacf4788d85
travisbrowning <travisbrowning@me.com>
2023-05-06 22:57:52 -0500

    Add style and basic functionality

---
+++ script.js
@@ -0,0 +1,12 @@
+let count = 890;
+let countEl = document.getElementById("count-el");
+
+function increment() {
+    count += 1
+    countEl.textContent = count;
+}
+
+function reset() {
+    count = 0;
+    countEl.textContent = 0;
+}
